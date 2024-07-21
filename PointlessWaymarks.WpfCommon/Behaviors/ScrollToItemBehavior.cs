1e839c6431bc959b75ba0ccda91de439168dbfe5
Charles Miles <charles@cmiles.info>
2024-07-21 10:00:53 -0700

    WpfCommon: Scroll To Item Behavior
    This provides a simple way to request an item scroll into view from a ViewModel. I think this might be more idiomatic as an event - but this provides such a clean mechanism that it seems valuable.

---
+++ PointlessWaymarks.WpfCommon/Behaviors/ScrollToItemBehavior.cs
@@ -0,0 +1,47 @@
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace PointlessWaymarks.WpfCommon.Behaviors;
+
+/// <summary>
+///     This behaviour watches the ScrollRequestItem property and when it changes it tries to scroll
+///     the item into view in the associated ListBox. This behavior tries to ignore any/all errors.
+///     The intended use is to bind the ScrollRequestItem to a property in a ViewModel and then only
+///     update the ViewModel property when you want to make a ScrollRequest -> this might be a more
+///     natural fit for an Event rather than a property binding so beware of the non-standard usage!
+/// </summary>
+public class ScrollToItemBehavior : Behavior<ListBox>
+{
+    //Inspiration from https://stackoverflow.com/questions/43763831/binding-the-delay-property-of-a-binding
+    public static readonly DependencyProperty ScrollRequestItemProperty =
+        DependencyProperty.Register(nameof(ScrollRequestItem), typeof(object),
+            typeof(ScrollToItemBehavior), new PropertyMetadata(null, OnScrollRequestItemChanged));
+
+    public object? ScrollRequestItem
+    {
+        get => (object?)GetValue(ScrollRequestItemProperty);
+        set => SetValue(ScrollRequestItemProperty, value);
+    }
+
+    private static void OnScrollRequestItemChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var source = d as ScrollToItemBehavior;
+        var attached = source?.AssociatedObject;
+        if (attached is null) return;
+        if (source!.ScrollRequestItem is null) return;
+
+        if (attached.Items.Count == 0) return;
+
+        foreach (var loopItems in attached.Items)
+            if (loopItems != null && loopItems.Equals(source.ScrollRequestItem))
+                try
+                {
+                    attached.ScrollIntoView(loopItems);
+                }
+                catch
+                {
+                    // ignored
+                }
+    }
+}
