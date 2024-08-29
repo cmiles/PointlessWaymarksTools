dee35f218f6764eaa5af5b3985df67c1331d05cd
Charles Miles <charles@cmiles.info>
2024-08-29 08:44:51 -0700

    WpfCommon: Markdown Help Window, Package Updates

---
+++ PointlessWaymarks.WpfCommon/MarkdownDisplay/HelpDisplayWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using PointlessWaymarks.LlamaAspects;
+using PointlessWaymarks.WpfCommon.Status;
+using PointlessWaymarks.WpfCommon.Utility;
+
+namespace PointlessWaymarks.WpfCommon.MarkdownDisplay;
+
+/// <summary>
+///     Interaction logic for HelpDisplayWindow.xaml
+/// </summary>
+[NotifyPropertyChanged]
+public partial class HelpDisplayWindow : Window
+{
+    public HelpDisplayWindow()
+    {
+        InitializeComponent();
+
+        DataContext = this;
+    }
+
+    public required HelpDisplayContext HelpContext { get; set; }
+    public required StatusControlContext StatusContext { get; set; }
+    public string WindowTitle { get; set; } = "Help and Information";
+
+    public static async Task CreateInstanceAndShow(List<string> markdown, string windowTitle)
+    {
+        await ThreadSwitcher.ResumeForegroundAsync();
+
+        var window = new HelpDisplayWindow
+        {
+            HelpContext = new HelpDisplayContext(markdown), StatusContext = new StatusControlContext(),
+            WindowTitle = windowTitle
+        };
+
+        await window.PositionWindowAndShowOnUiThread();
+    }
+}
