192a1ae70a0f3a42b60fe75a3505662f1c0bef86
Charles Miles <charles@cmiles.info>
2024-09-22 10:10:45 -0700

    CommonTools: More Logging in ProgramInfoTools when checking for installers
    Problems here lend themselves to checking logs rather than debugging, hence more logging with the increase in complexity of supporting a Json source as well as directory reference.

--- PointlessWaymarks.CommonTools/ProgramInfoTools.cs
+++ PointlessWaymarks.CommonTools/ProgramInfoTools.cs
@@ -31,37 +31,98 @@ public static class ProgramInfoTools
         return null;
     }
 
-    public static async Task<(string? dateString, string? setupFile)> LatestInstaller(string installerDirectory,
+    public static async Task<(string? dateString, string? setupFile)> LatestInstaller(string installerLocation,
         string baseInstallerName)
     {
-        if (string.IsNullOrEmpty(installerDirectory)) return (null, null);
-        if (string.IsNullOrEmpty(baseInstallerName)) return (null, null);
+        Log.Information("Latest Installer Called with Installer Location {0} and Base Installer Name {1}",
+            installerLocation, baseInstallerName);
 
-        if (installerDirectory.EndsWith(".json", StringComparison.OrdinalIgnoreCase))
+        if (string.IsNullOrEmpty(installerLocation))
         {
-            var jsonString = installerDirectory.StartsWith("http") ? await installerDirectory.GetStringAsync() : FileAndFolderTools.ReadAllText(installerDirectory);
-
-            if (string.IsNullOrEmpty(jsonString)) return (null, null);
-
-            var fileList = JsonSerializer.Deserialize<List<ProgramInfoJson>>(jsonString);
-            var latestInstaller = fileList?.Where(x => x.InstallerBaseName.Equals(baseInstallerName, StringComparison.OrdinalIgnoreCase)).MaxBy(x => x.ApplicationVersion);
-
-             if (latestInstaller?.ApplicationVersion is null) return (null, null);
-            return (latestInstaller.ApplicationVersion.Value.ToString("yyyy-MM-dd-HH-mm"), latestInstaller.InstallerUrl);
+            Log.Debug("LatestInstaller Called with an empty installerLocation");
+            return (null, null);
         }
 
-        var containingDirectory = new DirectoryInfo(installerDirectory);
+        if (string.IsNullOrEmpty(baseInstallerName))
+        {
+            Log.Debug("LatestInstaller Called with an empty baseInstallerName");
+            return (null, null);
+        }
 
-        if (!containingDirectory.Exists) return (null, null);
+        if (installerLocation.EndsWith(".json", StringComparison.OrdinalIgnoreCase))
+        {
+            string? jsonString;
+            if (installerLocation.StartsWith("http"))
+            {
+                Log.Debug("Installer Json File starting with http?:// detected - {0} - getting json.",
+                    installerLocation);
+
+                jsonString = await installerLocation.GetStringAsync();
+            }
+            else
+            {
+                Log.Debug("Installer Json File starting without http?:// detected - {0} - reading file.",
+                    installerLocation);
+                jsonString = FileAndFolderTools.ReadAllText(installerLocation);
+            }
+
+            if (string.IsNullOrEmpty(jsonString))
+            {
+                Log.Debug("Deserialized Installer Json File {0} is empty", installerLocation);
+                return (null, null);
+            }
+
+            var fileList = JsonSerializer.Deserialize<List<ProgramInfoJson>>(jsonString);
+            var latestInstaller = fileList
+                ?.Where(x => x.InstallerBaseName.Equals(baseInstallerName, StringComparison.OrdinalIgnoreCase))
+                .MaxBy(x => x.ApplicationVersion);
+
+            Log.ForContext(nameof(fileList), fileList.SafeObjectDump())
+                .ForContext(nameof(latestInstaller), latestInstaller.SafeObjectDump())
+                .Information("Found {0} entries in the Json Installer File", fileList?.Count ?? 0);
+
+            if (latestInstaller?.ApplicationVersion is null)
+            {
+                Log.Debug("No Latest Installer or Application Version Found for Latest Installer");
+                return (null, null);
+            }
+
+            Log.ForContext(nameof(latestInstaller), latestInstaller.SafeObjectDump()).Information(
+                "Installer Found - Version {0}, Installer {1}", latestInstaller.ApplicationVersion,
+                latestInstaller.InstallerUrl);
+
+            return (latestInstaller.ApplicationVersion.Value.ToString("yyyy-MM-dd-HH-mm"),
+                latestInstaller.InstallerUrl);
+        }
+
+        Log.Debug("Installer Directory Style Reference Detected - {0}", installerLocation);
+        var containingDirectory = new DirectoryInfo(installerLocation);
+
+        if (!containingDirectory.Exists)
+        {
+            Log.Debug("Installer Directory {0} does not Exist", installerLocation);
+            return (null, null);
+        }
 
         var publishFile = containingDirectory.GetFiles($"{baseInstallerName}--*.exe").ToList().MaxBy(x => x.Name);
 
-        if (publishFile == null) return (null, null);
+        if (publishFile == null)
+        {
+            Log.Debug("No Installer File Found in {0} for {1}", installerLocation, baseInstallerName);
+            return (null, null);
+        }
 
         var dateVersionString = Regex
             .Match(publishFile.Name, @".*--(?<dateVersion>\d\d\d\d-\d\d-\d\d-\d\d-\d\d).exe");
 
-        if (!dateVersionString.Groups.TryGetValue("dateVersion", out var group)) return (null, null);
+        if (!dateVersionString.Groups.TryGetValue("dateVersion", out var group))
+        {
+            Log.Debug("Installers found - {0} - but couldn't parse a Date Version from the File Name",
+                publishFile.FullName);
+            return (null, null);
+        }
+
+        Log.Information("Installer Found - Version {0}, Installer {1}", group.Value, publishFile.FullName);
 
         return (group.Value, publishFile.FullName);
     }
