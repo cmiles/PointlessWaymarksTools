c2fdcbb7da26682935f1f16d03a00c7e2aa80ab4
Charles Miles <charles@cmiles.info>
2024-11-28 08:06:33 -0700

    WpfCommon: Allow for naming in the WebViewToJpgFunctionInjection
    For scenarios where there are multiple WebView2s

--- PointlessWaymarks.WpfCommon/Behaviors/WebViewToJpgFunctionInjection.cs
+++ PointlessWaymarks.WpfCommon/Behaviors/WebViewToJpgFunctionInjection.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Windows;
 using Microsoft.Web.WebView2.Wpf;
 using Microsoft.Xaml.Behaviors;
 using OneOf;
@@ -9,28 +10,49 @@ namespace PointlessWaymarks.WpfCommon.Behaviors;
 
 public class WebViewToJpgFunctionInjection : Behavior<WebView2>
 {
+    public static readonly DependencyProperty JpgScreenshotFunctionNameProperty = DependencyProperty.Register(
+        nameof(JpgScreenshotFunctionName), typeof(string), typeof(WebViewToJpgFunctionInjection),
+        new PropertyMetadata("JpgScreenshotFunctionName", PropertyChangedCallback));
+
+    private WebView2? _webView;
+
+    public string JpgScreenshotFunctionName
+    {
+        get => (string)GetValue(JpgScreenshotFunctionNameProperty);
+        set => SetValue(JpgScreenshotFunctionNameProperty, value);
+    }
+
     protected override void OnAttached()
     {
         base.OnAttached();
 
         AssociatedObject.DataContextChanged += (sender, _) =>
         {
-            if (sender is WebView2 webView)
-            {
-                if (webView.DataContext is null) return;
+            _webView = sender as WebView2;
 
-                var dataContext = webView.DataContext;
-                if (dataContext != null)
-                {
-                    var property = dataContext.GetType().GetProperty("JpgScreenshotFunction",
-                        BindingFlags.Public | BindingFlags.Instance);
-                    if (property != null && property.PropertyType ==
-                        typeof(Func<Task<OneOf<Success<byte[]>, Error<string>>>>))
-                        property.SetValue(dataContext,
-                            new Func<Task<OneOf<Success<byte[]>, Error<string>>>>(() =>
-                                WebViewToJpg.SaveCurrentPageAsJpeg(webView)));
-                }
-            }
+            TryInjectFunction();
         };
+    }
+
+    private static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not WebViewToJpgFunctionInjection possibleContainer) return;
+
+        possibleContainer.TryInjectFunction();
+    }
+
+    private void TryInjectFunction()
+    {
+        var dataContext = _webView?.DataContext;
+
+        if (dataContext is null) return;
+
+        var property = dataContext.GetType().GetProperty(JpgScreenshotFunctionName,
+            BindingFlags.Public | BindingFlags.Instance);
+        if (property != null && property.PropertyType ==
+            typeof(Func<Task<OneOf<Success<byte[]>, Error<string>>>>))
+            property.SetValue(dataContext,
+                new Func<Task<OneOf<Success<byte[]>, Error<string>>>>(() =>
+                    WebViewToJpg.SaveCurrentPageAsJpeg(_webView!)));
     }
 }
