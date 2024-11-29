59bc7b0b0d13b4203ca55f6c8f91be51ed549757
Charles Miles <charles@cmiles.info>
2024-11-29 09:03:30 -0700

    WpfCommon: WebViewToJpg support for horizontal scrolling

--- PointlessWaymarks.WpfCommon/Utility/WebViewToJpg.cs
+++ PointlessWaymarks.WpfCommon/Utility/WebViewToJpg.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text.Json;
 using Microsoft.Web.WebView2.Core;
 using Microsoft.Web.WebView2.Wpf;
 using OneOf;
@@ -52,59 +53,124 @@ public static class WebViewToJpg
         var viewPortUserPosition = int.Parse(viewPortUserPositionString);
 
         await webContentWebView.CoreWebView2.ExecuteScriptAsync(
+            "document.querySelector('body').style.overflowY='visible'");
+        await webContentWebView.CoreWebView2.ExecuteScriptAsync(
             "document.querySelector('body').style.overflow='hidden'");
 
-        await webContentWebView.CoreWebView2.ExecuteScriptAsync("""
-                                                                var x = document.querySelectorAll('*');
-                                                                for(var i=0; i<x.length; i++) {
-                                                                    elementStyle = getComputedStyle(x[i]);
-                                                                    if(elementStyle.position=="fixed" || elementStyle.position=="sticky") {
-                                                                        x[i].style.position="absolute";
-                                                                    }
-                                                                }
-                                                                """);
+        var documentHeightArray = await webContentWebView.CoreWebView2.ExecuteScriptAsync(
+            """
+            [
+                document.documentElement.clientHeight,
+                document.body ? document.body.scrollHeight : 0,
+                document.documentElement.scrollHeight,
+                document.body ? document.body.offsetHeight : 0,
+                document.documentElement.offsetHeight
+            ]
+            """);
+        var documentHeight =
+            JsonSerializer.Deserialize<List<int>>(documentHeightArray)!.Max();
 
-        var imageViewportHeight =
+        var documentWidthArray = await webContentWebView.CoreWebView2.ExecuteScriptAsync(
+            """
+            [
+                document.documentElement.clientWidth,
+                document.body ? document.body.scrollWidth : 0,
+                document.documentElement.scrollWidth,
+                document.body ? document.body.offsetWidth : 0,
+                document.documentElement.offsetWidth
+            ]
+            """);
+        var documentWidth =
+            JsonSerializer.Deserialize<List<int>>(documentWidthArray)!.Max();
+
+        var viewportHeight =
             int.Parse(await webContentWebView.CoreWebView2.ExecuteScriptAsync("visualViewport.height"));
         var viewportWidth = int.Parse(await webContentWebView.CoreWebView2.ExecuteScriptAsync("visualViewport.width"));
-        var documentScrollHeight =
-            int.Parse(await webContentWebView.CoreWebView2.ExecuteScriptAsync("document.body.scrollHeight"));
 
-        var chunks = (int)Math.Ceiling((double)documentScrollHeight / imageViewportHeight);
-        var imageBytesList = new List<byte[]>();
-
-        for (var i = 0; i < chunks; i++)
+        if (documentHeight > viewportHeight)
         {
-            var scrollToTopFunction = $$"""
-                                        window.scrollTo({
-                                           top: {{i * imageViewportHeight}},
-                                           left: 0,
-                                           behavior: "instant"
-                                        });
-                                        """;
-
-            await webContentWebView.CoreWebView2.ExecuteScriptAsync(scrollToTopFunction);
-            await Task.Delay(1000);
-
-            using var stream = new MemoryStream();
-            await webContentWebView.CoreWebView2.CapturePreviewAsync(CoreWebView2CapturePreviewImageFormat.Png, stream);
-            var imageBytes = stream.ToArray();
-            imageBytesList.Add(imageBytes);
+            await webContentWebView.CoreWebView2.ExecuteScriptAsync("""
+                                                                    var x = document.querySelectorAll('*');
+                                                                    for(var i=0; i<x.length; i++) {
+                                                                        elementStyle = getComputedStyle(x[i]);
+                                                                        if(elementStyle.position=="fixed" || elementStyle.position=="sticky") {
+                                                                            x[i].style.position="absolute";
+                                                                        }
+                                                                    }
+                                                                    """);
+            documentHeight =
+                int.Parse(await webContentWebView.CoreWebView2.ExecuteScriptAsync("document.body.scrollHeight"));
         }
 
-        using var finalImage = new SKBitmap(viewportWidth, documentScrollHeight);
+
+        var verticalChunks = (int)Math.Ceiling((double)documentHeight / viewportHeight);
+        var verticalImageBytesList = new List<byte[]>();
+
+        var horizontalChunks = (int)Math.Ceiling((double)documentWidth / viewportWidth);
+
+        for (var i = 0; i < verticalChunks; i++)
+        {
+            using var rowImage = new SKBitmap(documentWidth, viewportHeight);
+            using var rowCanvas = new SKCanvas(rowImage);
+
+            var currentWidth = 0;
+
+            for (var j = 0; j < horizontalChunks; j++)
+            {
+                var scrollToViewFunction = $$"""
+                                            window.scrollTo({
+                                               top: {{i * viewportHeight}},
+                                               left: {{j * viewportWidth}},
+                                               behavior: "instant"
+                                            });
+                                            """;
+                await webContentWebView.CoreWebView2.ExecuteScriptAsync(scrollToViewFunction);
+                await Task.Delay(1000);
+                using var stream = new MemoryStream();
+                await webContentWebView.CoreWebView2.CapturePreviewAsync(CoreWebView2CapturePreviewImageFormat.Png,
+                    stream);
+                var imageBytes = stream.ToArray();
+
+                using var image = SKBitmap.Decode(imageBytes);
+
+                var sourceRect = new SKRect(0, 0, image.Width, image.Height);
+                var destRect = new SKRect(currentWidth, 0, currentWidth + image.Width, image.Height);
+
+                if (j == horizontalChunks - 1 && horizontalChunks > 1)
+                {
+                    var neededLastImageWidth = documentWidth % viewportWidth;
+                    destRect = new SKRect(currentWidth - (image.Width - neededLastImageWidth), 0, currentWidth + neededLastImageWidth, image.Height);
+
+                    currentWidth += neededLastImageWidth;
+                }
+                else
+                {
+                    currentWidth += image.Width;
+                }
+
+                rowCanvas.DrawBitmap(image, sourceRect, destRect);
+
+            }
+
+            using var rowImageEncoded = SKImage.FromBitmap(rowImage);
+            using var rowImageStream = new MemoryStream();
+            rowImageEncoded.Encode(SKEncodedImageFormat.Png, 100).SaveTo(rowImageStream);
+            verticalImageBytesList.Add(rowImageStream.ToArray());
+        }
+
+        using var finalImage = new SKBitmap(documentWidth, documentHeight);
         using var canvas = new SKCanvas(finalImage);
         var currentHeight = 0;
 
-        for (var i = 0; i < imageBytesList.Count; i++)
+        for (var i = 0; i < verticalImageBytesList.Count; i++)
         {
-            using var image = SKBitmap.Decode(imageBytesList[i]);
+            using var image = SKBitmap.Decode(verticalImageBytesList[i]);
             var sourceRect = new SKRect(0, 0, image.Width, image.Height);
             var destRect = new SKRect(0, currentHeight, image.Width, currentHeight + image.Height);
 
-            if (i == imageBytesList.Count - 1)
+            if (i == verticalImageBytesList.Count - 1)
             {
-                var neededLastImageHeight = documentScrollHeight % imageViewportHeight;
+                var neededLastImageHeight = documentHeight % viewportHeight;
                 destRect = new SKRect(0, currentHeight - (image.Height - neededLastImageHeight), image.Width,
                     currentHeight + neededLastImageHeight);
 
